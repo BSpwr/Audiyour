04fafc920986d842371def72f6eda74649662669
pankeleo <leonardoenriquetorres1@gmail.com>
2022-01-19 16:49:43 -0500

    Update main.dart

--- flutter_app/myapp/lib/main.dart
+++ flutter_app/myapp/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-//Peer Help: Helped Enemias get acquainted with running the Flutter App on a virtual machine
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Mixer GUI'),
     );
   }
 }
