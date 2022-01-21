238c5940fbf2e17553ed60fa85c3d5c69a79ab57
London Torres <londonztorres@gmail.com>
2022-01-21 01:18:09 -0500

    Add mixer control
    - Add mixer sliders to GUI
    - Add mixer functions to bluetoothManager
    - Format files

--- gui_qt/mixer.py
+++ gui_qt/mixer.py
@@ -5,15 +5,19 @@ import asyncio
 from jumpSlider import JumpSlider
 
 
-class Equalizer(Qw.QWidget):
+class Mixer(Qw.QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.freq_bands = [31, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000]
+        self.tracks = ['3.5mm Jack', 'Bluetooth']
         self.band_widgets = []
 
-        self.set_enable_btn = Qw.QPushButton("Enable Equalizer")
-        self.set_enable_btn.setCheckable(True)
-        self.set_enable_btn.toggled.connect(self.set_equalizer_enable)
+        self.jack_enable_btn = Qw.QPushButton("Enable 3.5mm Jack Input")
+        self.jack_enable_btn.setCheckable(True)
+        self.jack_enable_btn.toggled.connect(self.jack_mixer_enable)
+
+        self.wireless_enable_btn = Qw.QPushButton("Enable Bluetooth Input")
+        self.wireless_enable_btn.setCheckable(True)
+        self.wireless_enable_btn.toggled.connect(self.wireless_mixer_enable)
 
         self.set_defaults_btn = Qw.QPushButton("Set Defaults")
         self.set_defaults_btn.clicked.connect(self.set_defaults)
@@ -22,17 +26,15 @@ class Equalizer(Qw.QWidget):
         self.load_settings_btn.clicked.connect(lambda: asyncio.ensure_future(self.load_settings()))
 
         self.toolbar_layout = Qw.QHBoxLayout()
-        self.toolbar_layout.addWidget(self.set_enable_btn)
+        self.toolbar_layout.addWidget(self.jack_enable_btn)
+        self.toolbar_layout.addWidget(self.wireless_enable_btn)
         self.toolbar_layout.addWidget(self.set_defaults_btn)
         self.toolbar_layout.addWidget(self.load_settings_btn)
 
         self.bands_layout = Qw.QHBoxLayout()
 
-        for idx, b_freq in enumerate(self.freq_bands):
-            if b_freq > 999:
-                band_w = EqualizerBand(f'{int(b_freq / 1000) if b_freq % 1000 == 0 else (b_freq / 1000)} kHz', parent=self)
-            else:
-                band_w = EqualizerBand(f'{b_freq} Hz', parent=self)
+        for idx, b_freq in enumerate(self.tracks):
+            band_w = MixerBand(b_freq, parent=self)
 
             band_w.slider.valueChanged.connect(lambda x, idx=idx: asyncio.ensure_future(self.do_band_update(idx, dB=x)))
 
@@ -49,8 +51,8 @@ class Equalizer(Qw.QWidget):
 
 
     async def load_settings(self):
-        await self.parent().parent().bt_man.read_eq_gains()
-        gains = self.parent().parent().bt_man.eq_gains
+        await self.parent().parent().bt_man.read_mix_gains()
+        gains = self.parent().parent().bt_man.mix_gains
 
         for idx, band_w in enumerate(self.band_widgets):
             band_w.slider.blockSignals(True)
@@ -60,11 +62,18 @@ class Equalizer(Qw.QWidget):
         # self.set_enable_btn.setChecked(status)
 
 
-    def set_equalizer_enable(self):
-        if self.set_enable_btn.isChecked():
-            self.set_enable_btn.setText("Disable Equalizer")
+    def jack_mixer_enable(self):
+        if self.jack_enable_btn.isChecked():
+            self.jack_enable_btn.setText("Disable 3.5mm Jack Input")
         else:
-            self.set_enable_btn.setText("Enable Equalizer")
+            self.jack_enable_btn.setText("Enable 3.5mm Jack Input")
+
+
+    def wireless_mixer_enable(self):
+        if self.wireless_enable_btn.isChecked():
+            self.wireless_enable_btn.setText("Disable Bluetooth Input")
+        else:
+            self.wireless_enable_btn.setText("Enable Bluetooth Input")
 
 
     def set_defaults(self):
@@ -74,20 +83,20 @@ class Equalizer(Qw.QWidget):
             band_w.update_db_value(0)
             band_w.slider.blockSignals(False)
 
-        asyncio.ensure_future(self.parent().parent().bt_man.write_eq_gains([0 for i in range(0, 10)]))
+        asyncio.ensure_future(self.parent().parent().bt_man.write_mix_gains([0 for i in range(0, 2)]))
 
 
     async def do_band_update(self, band_num, dB):
-        await self.parent().parent().bt_man.write_eq_gain_index(band_num, int(dB))
+        await self.parent().parent().bt_man.write_mix_gain_index(band_num, int(dB))
         self.parent().parent().update_conn_status()
 
 
-class EqualizerBand(Qw.QWidget):
-    def __init__(self, center_freq: str, parent=None):
+class MixerBand(Qw.QWidget):
+    def __init__(self, label: str, parent=None):
         super().__init__(parent)
-        self.center_freq = center_freq
+        self.label = label
 
-        self.slider = JumpSlider(Qc.Qt.Vertical, parent=self)
+        self.slider = JumpSlider(Qc.Qt.Horizontal, parent=self)
         self.slider.setMinimum(-20)
         self.slider.setMaximum(10)
         self.slider.setSingleStep(1)
@@ -95,28 +104,28 @@ class EqualizerBand(Qw.QWidget):
         self.slider.setTickInterval(1)
         self.slider.setTickPosition(Qw.QSlider.TicksBothSides)
         self.slider.setSizePolicy(
-            Qw.QSizePolicy.Fixed, Qw.QSizePolicy.Expanding)
+            Qw.QSizePolicy.Expanding, Qw.QSizePolicy.Fixed)
         self.slider.valueChanged.connect(lambda val: self.update_db_value(val))
 
         self.db_value_label = Qw.QLabel("0 dB")
-        self.db_value_label.setFixedWidth(40)
+        self.db_value_label.setFixedWidth(60)
         self.db_value_label.setAlignment(Qc.Qt.AlignVCenter)
         self.db_value_label.setSizePolicy(
             Qw.QSizePolicy.Fixed, Qw.QSizePolicy.Fixed)
 
-        self.freq_text_box = Qw.QLabel(self.center_freq, parent=self)
-        self.freq_text_box.setAlignment(Qc.Qt.AlignVCenter)
-        self.freq_text_box.setSizePolicy(
+        self.label_text_box = Qw.QLabel(self.label, parent=self)
+        self.label_text_box.setAlignment(Qc.Qt.AlignVCenter)
+        self.label_text_box.setSizePolicy(
             Qw.QSizePolicy.Fixed, Qw.QSizePolicy.Fixed)
 
         self.layout = Qw.QVBoxLayout()
         self.layout.addWidget(self.db_value_label)
         self.layout.addWidget(self.slider)
-        self.layout.addWidget(self.freq_text_box)
+        self.layout.addWidget(self.label_text_box)
         self.layout.setAlignment(Qc.Qt.AlignVCenter)
         self.setLayout(self.layout)
         self.show()
-  
+
 
     def update_db_value(self, value: int):
         self.db_value_label.setText(f'{value} dB')
